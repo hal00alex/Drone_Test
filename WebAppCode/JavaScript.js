5a3dc1ac94c7fb9e45fcbf9070ff29eea2b9be51
Haley Alexander <hal.00.alexander@gmail.com>
2017-01-31 19:49:22 -0600

    Create JavaScript.js

---
+++ WebAppCode/JavaScript.js
@@ -0,0 +1,25 @@
+/*Change to team colors later*/ 
+h1{
+  text-align: center; 
+  font-family: Times New Roman; 
+  font-size: 32pt; 
+}
+h2{
+  text-align: center; 
+  font-family: Times New Roman; 
+  font-size: 30pt; 
+}
+p{
+  text-align: center; 
+  font-size: 24pt; 
+  font-family: Times New Roman; 
+}
+body{
+  /*used twitch purple for text*/ 
+  /*hyperlinked usernames*/ 
+  /*pink ground to complement purple and blue*/ 
+  text-align: center; 
+  font-size: 28pt; 
+  color: #6441a5; 
+  background-color: #ffccff;
+}
