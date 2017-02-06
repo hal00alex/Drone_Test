614861071dcae42b090ee30fbc018664e23e92c2
Haley Alexander <hal.00.alexander@gmail.com>
2017-02-06 15:00:03 -0600

    Update Update.py
    Python code for editing an htm file

--- WebAppCode/Update.py
+++ WebAppCode/Update.py
@@ -1,1 +1,12 @@
+import webbrowser 
+#write-html.py 
+f = open('helloworld.html', 'w') 
+message = """<html> 
+<head></head>
+<body><p>Drone Dis-Play</p></body>
+</html> """ 
 
+f.write(message) 
+f.close()
+
+webbrowser.open_new_tab('helloworld.html')
